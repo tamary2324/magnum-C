edebe39abe8ea2dbf7681cc40feb2d3c8dabe493
Arthur Mouginot <mouginot.arthur@gmail.com>
2025-10-08 09:56:26 +0200

    new test func magc_py

--- magc_py.c
+++ magc_py.c
@@ -1,12 +1,27 @@
 #include <Python.h>
 
+int Cfib(int n){
+    if (n < 2)
+        return n;
+    else
+        return Cfib(n-1)+Cfib(n-2);
+}
+
 static PyObject* hello(PyObject* self, PyObject* args) {
     printf("Hello from C! *testfunc*\n");
     Py_RETURN_NONE;
 }
 
+static PyObject* fib(PyObject* self, PyObject* args) {
+    int n;
+    if(!PyArg_ParseTuple(args, "i", &n)) // "i" tells Python.h to expect a single integer
+        return NULL;
+    return Py_BuildValue("i", Cfib(n));
+}
+
 static PyMethodDef Methods[] = {
     {"hello", hello, METH_NOARGS, "Print hello"},
+    {"fib", fib, METH_VARARGS, "calculate fib"},
     {NULL, NULL, 0, NULL}
 };
 
