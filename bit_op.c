063d8d1df9b6d86d109addef0d7f52f83ab9aa88
tamary2324 <tamariel2324@gmail.com>
2025-10-30 21:02:23 +0100

    add bit_op file with functions to get or set a bit in a uint8_t array

---
+++ bit_op.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdint.h>
+
+//faire passer le magnum plutôt que juste la value ?
+//bits are numeroted that way :
+//[012345678 , 9..., 17..., ...]
+
+uint8_t _get_bit_magnum(uint8_t* m_value, int i){
+    //get the ith bit of an uint8 array
+    if (m_value == NULL)
+        abort;
+
+    return (m_value[i/8] & (1<<(7-i%8))) >> (7-i%8) & 1;
+}
+
+void _set_bit_magnum(uint8_t* m_value, int i, int new_val){
+    //set the ith bit of an uint8 array with new_val
+    //new_val have to be 0 or 1
+    if (m_value == NULL)
+        abort;
+
+    m_value[i/8] = (m_value[i/8] & ~(1<<(7-(i%8)))) + (new_val<<(7-(i%8)));
+}
---
+++ bit_op.h
@@ -0,0 +1,5 @@
+#ifndef _BIT_OP_
+#define _BIT_OP_
+uint8_t _get_bit_magnum(uint8_t *m_value, int i);
+void _set_bit_magnum(uint8_t *m_value, int i, int new_val);
+#endif
