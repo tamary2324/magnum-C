27a4b9046584bee47f1a79b36b1a33cd4595be05
tamary2324 <tamariel2324@gmail.com>
2025-11-03 22:01:06 +0100

    adds the possibility to work with even smaller double numbers (until 2^-1074)

--- convert.c
+++ convert.c
@@ -58,8 +58,15 @@ int from_magnum_to_int(struct magnum * magnum){
 
 struct magnum * to_magnum_from_double(double a){ 
     //get an double and return the adress of the corresponding magnum structure allocated on the stack
-    int64_t a_as_int = *(int64_t*)&a;
     struct magnum * mag1 = (struct magnum*) malloc(sizeof(struct magnum));
+
+    if (a==0.){
+        mag1->power = 0;
+        mag1->sign_n_prec = 0;
+        mag1->value = (uint8_t *) malloc(sizeof(uint8_t));
+        mag1->value[0] = 0;
+        return mag1;
+    }
 
     if (a<0.)
         mag1->sign_n_prec = -7;
@@ -71,8 +78,9 @@ struct magnum * to_magnum_from_double(double a){
         mag1->value[i] = 0;
     }
 
+    int64_t a_as_int = *(int64_t*)&a;
     int64_t a_power = ((a_as_int >> (int64_t)52) & (int64_t)2047) - 1023;
-
+    
     if (a_power != -1023){
         mag1->power = a_power / 8 - 6;
         int power_correction = (a_power % 8);
@@ -88,11 +96,29 @@ struct magnum * to_magnum_from_double(double a){
         for (int i = 0; i < 53; i++){
             _set_bit_magnum(mag1->value, 59 - i - power_correction, (a_value >> (int64_t)(i)) & 1);
         }
-        // print_magnum_info(mag1);
-        // printf("a_power = %d\n", a_power);
-        // printf("mag1->power = %d\n", mag1->power);
-        // printf("power_correction = %d\n\n", power_correction);
     }
+
+    else {
+        int64_t decalage = 0;
+        while (a_as_int >> ((int64_t)51 - decalage) == 0){
+            decalage++;
+        }
+        
+        a_power -= decalage;
+        mag1->power = a_power / 8 - 6;
+
+        int power_correction = (a_power % 8);
+        if (power_correction<0){ //normalize correction
+            power_correction = 8+power_correction;
+            mag1->power--;
+        }
+
+        int64_t a_value = ~((int64_t)4095 << (int64_t)52) & a_as_int;
+        for (int i = 0; i < 52-decalage; i++){
+            _set_bit_magnum(mag1->value, 58 - i - power_correction - decalage, (a_value >> (int64_t)(i)) & 1);
+        }
+    }
+
     clean_magnum(mag1);
     return mag1;
 }
@@ -101,34 +127,48 @@ struct magnum * to_magnum_from_double(double a){
 double from_magnum_to_double(struct magnum * magnum){
     //get a magnum structure adress and return the corresponding double 
     //if it's not possible it return 0 and a consol message
-    int64_t value = 0;
 
-    if (magnum->power+abs(magnum->sign_n_prec)>128){
-        printf("the number is too big to be converted in float\n");
-        return 0.;
-    }
-    if (magnum->power+abs(magnum->sign_n_prec)<-133){
-        printf("the number is too small to be converted in float\n");
-        return 0.;
-    }
     if (magnum->value[0]==0)
         clean_magnum(magnum);
+
     
     int power_correction = 0;
     while (_get_bit_magnum(magnum->value,power_correction)!=1){
         power_correction++;
     }
-    power_correction++; //skip mantissa
 
-    for (int i = 0; i < 52; i++){
-        if (power_correction + i >= abs(magnum->sign_n_prec) * 8)
-            break;
+    int64_t value = 0;
 
-        value += (int64_t)_get_bit_magnum(magnum->value, i + power_correction) << (int64_t)(51-i);
+    int double_power = (magnum->power + abs(magnum->sign_n_prec)) * 8 - power_correction + 1022;
+    printf("double_power = %d\n", double_power);
+    if (double_power>=2047){
+        printf("the number is too big to be converted in float\n");
+        return 0.;
+    }
+    if (double_power<-51){
+        printf("the number is too small to be converted in float\n");
+        return 0.;
     }
 
-    int double_power = (magnum->power + abs(magnum->sign_n_prec)) * 8 - power_correction + 1023;
-    value += ((int64_t)double_power << (int64_t)52);
+    if (double_power>0){
+        power_correction++; //skip mantissa
+        value += ((int64_t)double_power << (int64_t)52);
+        for (int i = 0; i < 52; i++){
+            if (power_correction + i >= abs(magnum->sign_n_prec) * 8)
+                break;
+
+            value += (int64_t)_get_bit_magnum(magnum->value, i + power_correction) << (int64_t)(51-i);
+        }
+    }
+    else{
+        printf("pow c = %d\n", power_correction);
+        for (int i = 0; i < 52 + double_power; i++){
+            if (power_correction + i >= abs(magnum->sign_n_prec) * 8)
+                break;
+
+            value+=(int64_t)_get_bit_magnum(magnum->value, i+power_correction) << (int64_t)(51-i+double_power);
+        }
+    }
 
     double value_as_double = *((double *)&value);
 
